8ea576a262e1fb9424c8d50c28807fcb16c1557d
hkkang <hkkang@nextree.io>
2020-03-19 11:44:41 +0900

    템플릿 구조 변경

--- packages/react-scripts/bin/react-scripts.js
+++ packages/react-scripts/bin/react-scripts.js
@@ -19,14 +19,14 @@ const spawn = require('react-dev-utils/crossSpawn');
 const args = process.argv.slice(2);
 
 const scriptIndex = args.findIndex(
-  x => x === 'build' ||  x === 'build_app' ||  x === 'build_lib' ||  x === 'eject' || x === 'start' || x === 'test'
+  x => x === 'build' ||  x === 'build-app' ||  x === 'build-lib' ||  x === 'eject' || x === 'start' || x === 'test'
 );
 const script = scriptIndex === -1 ? args[0] : args[scriptIndex];
 const nodeArgs = scriptIndex > 0 ? args.slice(0, scriptIndex) : [];
 
 switch (script) {
-  case 'build_app':
-  case 'build_lib':
+  case 'build-app':
+  case 'build-lib':
   case 'eject':
   case 'start':
   case 'test': {
